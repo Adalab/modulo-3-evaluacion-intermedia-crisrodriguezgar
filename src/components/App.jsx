02963c3bf922c977eee532130d5bb3fcedab2e9c
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2023-10-16 11:04:29 +0200

    added add new countries

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,24 @@
 //imports dependencias, imagenes, componentes, stylos
 
 import callToApi from '../services/api';
-import '../styles/App.scss';
+import '../styles/index.scss';
 import {useState, useEffect} from 'react';
 
 function App() {
   //funciones, variables, handles,
-  const [listCountry, setCountry] = useState([]);
+  const [listCountry, setListCountry] = useState([]);
   const [search, setSearch] = useState('');
+  const [continent, setContinent] = useState('all');
+  const [newCountry, setNewCountry] = useState({
+    flag: '',
+    name: '',
+    capital: '',
+    continents: '',
+  });
 
   useEffect(() => {
     callToApi().then((dataApi) => {
-      setCountry(dataApi);
+      setListCountry(dataApi);
       console.log(dataApi);
     });
   }, []);
@@ -19,20 +26,46 @@ function App() {
   const handleForm = (ev) => {
     ev.preventDefault();
   };
+
   const handleInputSearch = (ev) => {
     setSearch(ev.target.value);
   };
 
+  const handleSelectSearch = (ev) => {
+    setContinent(ev.target.value);
+  };
+  const handleInputAdd = (ev) => {
+    console.log ("estoy añadiendo")
+    setNewCountry({...newCountry, [ev.target.id]: ev.target.value});
+  };
+  const handleNewCountry = () => {
+    console.log ("estoy haciendo click")
+    setNewCountry({
+      flag: '',
+      name: '',
+      capital: '',
+      continents: '',
+    });
+    setListCountry([...listCountry, newCountry]);
+  };
+
   const renderListCountry = () => {
     return listCountry
-      .filter(
-        (item) =>
-          item.name.common.toLowerCase().includes(search.toLowerCase()) 
+      .filter((item) => {
+        if (continent === 'all') {
+          return true;
+        } else {
+          return item.continents === continent;
+        }
+      })
+
+      .filter((item) =>
+        item.name.official.toLowerCase().includes(search.toLowerCase())
       )
       .map((item, i) => (
-        <li key={i}>
-          <p>{item.flag}</p>
-          <p>{item.name.common}</p>
+        <li className="list__country" key={i}>
+          <span>{item.flag}</span>
+          <h3>{item.name.official}</h3>
           <p>{item.capital}</p>
           <p>{item.continents}</p>
         </li>
@@ -42,16 +75,16 @@ function App() {
   //html
   return (
     <>
-      <header>
-        <h1>Country Info App</h1>
-        <p>
+      <header className="header">
+        <h1 className="header__title">Country Info App</h1>
+        <p className="header__subtitle">
           Explore information about countries, capitals, and flags. Add new
           countries and filter trough the list
         </p>
       </header>
       <main>
         <h3>Filters</h3>
-        <form action="" onSubmit={handleForm}>
+        <form className="form" action="" onSubmit={handleForm}>
           <label htmlFor="">By Country</label>
           <input
             type="text"
@@ -62,8 +95,13 @@ function App() {
           />
 
           <label htmlFor="">By Continent</label>
-          <select name="" id="">
-            <option value="">All</option>
+          <select
+            name="continent"
+            id="continent"
+            value={continent}
+            onChange={handleSelectSearch}
+          >
+            <option value="all">All</option>
             <option value="africa">Africa</option>
             <option value="northAmerica">North America</option>
             <option value="southAmerica">South America</option>
@@ -71,9 +109,44 @@ function App() {
             <option value="europe">Europe</option>
             <option value="oceania">Oceanía</option>
           </select>
+
+          <label htmlFor="">Add Country</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Country name"
+            value={newCountry.name}
+            onChange={handleInputAdd}
+          />
+          <input
+            type="text"
+            name="capital"
+            id="capital"
+            placeholder="Capital"
+            value={newCountry.capital}
+            onChange={handleInputAdd}
+          />
+          <input
+            type="text"
+            name="flag"
+            id="flag"
+            placeholder="Flag Icon"
+            value={newCountry.flag}
+            onChange={handleInputAdd}
+          />
+          <input
+            type="text"
+            name="continent"
+            id="ccontinent"
+            placeholder="Continent"
+            value={newCountry.continents}
+            onChange={handleInputAdd}
+          />
+          <button onClick={handleNewCountry}>Add Country</button>
         </form>
 
-        <ul>{renderListCountry()}</ul>
+        <ul className="list">{renderListCountry()}</ul>
       </main>
     </>
   );
